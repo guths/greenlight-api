3210723af96ea78a24ff389dadfc15f8da883c77
jguths <jonathanguths@gmail.com>
2023-09-19 20:28:32 -0300

    creating validator package

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,11 +1,15 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type Runtime int32
+
+var ErrInvalidRunTimeFormat = errors.New("invalid runtime format")
 
 func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
@@ -13,4 +17,28 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedJSONValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedValue, err := strconv.Unquote(string(jsonValue))
+
+	if err != nil {
+		return ErrInvalidRunTimeFormat
+	}
+
+	parts := strings.Split(unquotedValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRunTimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+
+	if err != nil {
+		return ErrInvalidRunTimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
